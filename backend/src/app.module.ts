58df3ef3c8e57090ecc741a184d7c786a2aff6d8
Ivan <ivanchernetskey@mail.ru>
2023-07-19 12:48:06 +0300

    feat: added possibility to save on disk information from redis and persists redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -19,7 +19,7 @@ import { redisStore } from 'cache-manager-redis-store';
       store: async () => await redisStore({
         // Store-specific configuration:
         socket: {
-          host: 'localhost',
+          host: 'redis-stack',
           port: 6379,
         }
       })
